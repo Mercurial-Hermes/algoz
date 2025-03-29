3002d8df97e7488c1a8d7c864de45fc92b2ab247
elusivenode <elusivenode@gmail.com>
2025-03-29 16:19:16 +1000

    testing

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const insertion_sort = @import("algorithms/sorting/insertion_sort.zig");
 
 pub fn main() !void {
     // Example array to sort
-    var numbers = [_]i32{ 64, 34, 25, 12, 22, 11, 90 };
+    var numbers = [_]i32{ 64, 34, 25, 12, 22, 11, 90, -4};
     
     std.debug.print("Original array: ", .{});
     for (numbers) |n| {
